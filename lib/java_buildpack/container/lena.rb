355ee9e671a5b882ce6c55288865362a1c72466f
원송연 책임 아키텍처솔루션팀 (songyeon.won@lgcns.com <76882@CORP.LGCNS.COM>
2020-02-07 10:59:57 +0900

    Update lena.rb

--- lib/java_buildpack/container/lena.rb
+++ lib/java_buildpack/container/lena.rb
@@ -68,7 +68,7 @@ module JavaBuildpack
           @droplet.environment_variables.as_env_vars,
           @droplet.java_home.as_env_var,
           'exec',
-          "$PWD/#{(@droplet.sandbox + 'servers/appServer/start.sh').relative_path_from(@droplet.root)}",
+          "$PWD/#{(@droplet.sandbox + 'entrypoint.sh').relative_path_from(@droplet.root)}",
           'run'
         ].flatten.compact.join(' ')
 
